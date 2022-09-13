e8cb3f86b2d0b83e9f23e2a4aa19329ed4fb7384
Onurhan Ak <onurhanak@protonmail.com>
2022-09-13 15:06:50 -0400

    added logger

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const {app, BrowserWindow} = require('electron')  
 const {spawn,exec} = require('child_process');
-
+const log = require('electron-log');
 // initialize the window variable
 let win  
 
 async function createWindow() { 
     // spawn the flask app
     const python = spawn('python', ['app_adera.py']);
-
+    log.info('Spawned python process.');
     await new Promise(r => setTimeout(r, 2000)); //sometimes it takes a while for flask server to start, this is to make sure the server
                                                 // is running before we launch the window. This is a TEMPORARY fix, we need to implements this as a callback.
 
     // configure the electron window
     win = new BrowserWindow({width: 1000, height: 800,autoHideMenuBar: true}) 
-    
+    log.info('Created electron window.')
     // open the intro route
     win.loadURL("http://127.0.0.1:5000/")
+    log.info('Opening local server')
 }
 
 function terminateAll() {
